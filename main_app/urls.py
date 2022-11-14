b304639eb85f63717da74489bf82f4fb82c0607f
kelso-gh <soyouwontsingg@gmail.com>
2022-11-14 18:57:01 -0500

    completed finch/index.html

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'), # Naming routes is best practice 
     path('about/', views.about, name='about'),
-]
+    path('finches/', views.finches_index, name="finches")
+]   
