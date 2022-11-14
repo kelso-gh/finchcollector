a6451818a93244fd6a68cac1f5bbcdd7affc22b0
kelso-gh <soyouwontsingg@gmail.com>
2022-11-14 17:36:29 -0500

    initial commit

---
+++ main_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'), # Naming routes is best practice 
+    path('about/', views.about, name='about'),
+]
---
+++ main_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+finches = [ 
+    {'name': 'Evening Grosbeak', 'habitat': 'Northern and Montane forests', 'threats': 'Deforestation'},
+    {'name': 'Pine Grosbeak', 'habitat': 'Open boreal forest', 'threats': 'Possibly climate change'},
+    {'name': 'Gray-crowned Rosy-Finch', 'habitat': 'Alpine tundra', 'threats': 'Climate change'},
+]
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
