e1bb9431c79bb1ef1b961fbd48523c10ba91391a
kelso-gh <soyouwontsingg@gmail.com>
2022-11-15 14:45:04 -0500

    models.py and data

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    habitat = models.CharField(max_length=100)
+    threats = models.TextField(max_length=100) #text box!
+    
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
 
-finches = [ 
-    {'name': 'Evening Grosbeak', 'habitat': 'Northern and Montane forests', 'threats': 'Deforestation'},
-    {'name': 'Pine Grosbeak', 'habitat': 'Open boreal forest', 'threats': 'Possibly climate change'},
-    {'name': 'Gray-crowned Rosy-Finch', 'habitat': 'Alpine tundra', 'threats': 'Climate change'},
-]
 
 def home(request):
     return render(request, 'home.html')
