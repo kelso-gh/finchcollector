4d44b3c7d10a3dcc1944a0ccf63381a2216c6006
kelso-gh <soyouwontsingg@gmail.com>
2022-11-15 17:41:13 -0500

    Finch details UI/route complete

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'), # Naming routes is best practice 
     path('about/', views.about, name='about'),
-    path('finches/', views.finches_index, name="finches")
+    path('finches/', views.finches_index, name="finches"),
+    path('finches/<int:finch_id>/', views.finches_detail, name='detail'),
 ]   
