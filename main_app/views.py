c8b4d74c75ec27b632d99955c4f75cd1e6f84a07
kelso-gh <soyouwontsingg@gmail.com>
2022-11-16 17:14:01 -0500

    Update views, urls, html complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 
@@ -21,3 +21,11 @@ def finches_detail(request, finch_id):
 class FinchCreate(CreateView):
     model = Finch
     fields = '__all__'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['habitat', 'threats']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches'
